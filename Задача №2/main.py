0b1ba385c0cd67d33d4da883b089e7a88ea4853c
Basil-Slinko <86016038+Basil-Slinko@users.noreply.github.com>
2021-10-07 11:11:07 +0300

    Update main.py

--- Задача №2/main.py
+++ Задача №2/main.py
@@ -4,8 +4,7 @@ import hashlib
 def returning_a_hash_of_strings(path_to_file):
     with open(path_to_file, "r", encoding='utf-8') as file:
         for line in file:
-            line_data = hashlib.md5(line.encode())
-            line_hash = line_data.hexdigest()
+            line_hash = hashlib.md5(line.encode()).hexdigest()
             yield line_hash
 
 
