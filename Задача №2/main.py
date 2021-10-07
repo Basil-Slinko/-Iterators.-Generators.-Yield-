5baacb1b53b3df16e4454bb72b7cf914223a0687
Basil-Slinko <86016038+Basil-Slinko@users.noreply.github.com>
2021-10-07 11:02:15 +0300

    Add files via upload

---
+++ Задача №2/main.py
@@ -0,0 +1,14 @@
+import hashlib
+
+
+def returning_a_hash_of_strings(path_to_file):
+    with open(path_to_file, "r", encoding='utf-8') as file:
+        for line in file:
+            line_data = hashlib.md5(line.encode())
+            line_hash = line_data.hexdigest()
+            yield line_hash
+
+
+if __name__ == '__main__':
+    for line in returning_a_hash_of_strings('new_document.txt'):
+        print(line)
