c15c3f9e8fca33bd7e10225efb5146adc7e9e10a
Basil-Slinko <86016038+Basil-Slinko@users.noreply.github.com>
2021-10-24 16:54:20 +0300

    Update main.py

--- Задача №1/main.py
+++ Задача №1/main.py
@@ -2,30 +2,32 @@ import json
 
 
 class CountriesIterate:
+  
+  def __init__(self, path_json, list_names_and_links=list()):
+    self.file = open(path_json)
+    self.list_names_and_links = list_names_and_links
 
-    def __init__(self, path_json, path_list_links):
-        self.file = open(path_json)
-        self.path_list_links = path_list_links
+  def __iter__(self):
+    self.cursor = -1
+    self.get_name_country()
+    return self
 
-    def __iter__(self):
-        self.get_name_country()
-        return self
+  def get_name_country(self):
+    countries_json = self.file.read()
+    list_countries = json.loads(countries_json)
+    for country in list_countries:
+      name_country = country['name']['common'].replace(" ", "_")
+      name_and_link_country = f"{country['name']['common']} – {'https://en.wikipedia.org/wiki/' + name_country}\n"
+      self.list_names_and_links.append(name_and_link_country)
 
-    def get_name_country(self):
-        countries_json = self.file.read()
-        list_countries = json.loads(countries_json)
-        for country in list_countries:
-            name_country = country['name']['common'].replace(" ", "_")
-            name_and_link_country = f"{country['name']['common']} – {'https://en.wikipedia.org/wiki/' + name_country}\n"
-            with open(self.path_list_links, 'a', encoding='UTF-8') as country_links_file:
-                country_links_file.write(f'{name_and_link_country}')
-
-    def __next__(self):
-        if self.__iter__() is None:
-            raise StopIteration
-        return self
+  def __next__(self):
+    self.cursor += 1
+    if self.cursor == len(self.list_names_and_links):
+        raise StopIteration
+    link = self.list_names_and_links[self.cursor]
+    return link
 
 
-if __name__ == '__main__':
-    pr = CountriesIterate('countries.json', 'countries_list.txt')
-    pr.__iter__()
+with open('country_links.txt', 'a') as country_links_file:
+  for country_link in CountriesIterate('countries.json'):
+    country_links_file.write(f'{country_link}')
